437a6420aba1580152d9fd88edc39a1907704c31
TheBossUsername <scot226@live.com>
2021-12-13 21:58:30 -0800

    Update __main__.py

--- Final Project/Hunt_The_Beast/__main__.py
+++ Final Project/Hunt_The_Beast/__main__.py
@@ -29,8 +29,12 @@ def main(screen):
     for x in range(7, 100, 14):
         for y in range(2, 30, 4):
             i += 1
+            z = 0
             for t in range(0, 8, 1):
-                position = Point(x + t, y - (2 * (i % 3)))
+                position = Point(x + t, y - (2 * z))
+                if i ==3:
+                    z += 1
+                    i = 0
                 brick = Actor()
                 brick.set_text("~")
                 brick.set_position(position)
